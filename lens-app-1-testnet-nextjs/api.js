43c127e77462f4ca56e82a1c62eef2bf7fafd194
raphadeluca <raphadeluca@protonmail.com>
2022-06-29 17:37:24 +0200

    fetch profiles

---
+++ lens-app-1-testnet-nextjs/api.js
@@ -0,0 +1,117 @@
+import { createClient } from "urql";
+
+export const client = createClient({
+  url: "https://api-mumbai.lens.dev",
+});
+
+export const RECOMMENDED_PROFILES = `
+    query {
+        recommendedProfiles {
+            id
+            name
+            bio
+            attributes {
+                displayType
+                traitType
+                key
+                value
+            }
+            metadata
+            isDefault
+            picture {
+            ... on NftImage {
+                contractAddress
+                tokenId
+                uri
+                verified
+            }
+            ... on MediaSet {
+                original {
+                url
+                width
+                height
+                mimeType
+                }
+                small {
+                url
+                width
+                height
+                mimeType
+                }
+                medium {
+                url
+                width
+                height
+                mimeType
+                }
+            }
+            __typename
+            }
+            handle
+            coverPicture {
+            ... on NftImage {
+                contractAddress
+                tokenId
+                uri
+                verified
+            }
+            ... on MediaSet {
+                original {
+                url
+                width
+                height
+                mimeType
+                }
+                small {
+                height
+                width
+                url
+                mimeType
+                }
+                medium {
+                url
+                width
+                height
+                mimeType
+                }
+            }
+            __typename
+            }
+            ownedBy
+            dispatcher {
+            address
+            canUseRelay
+            }
+            stats {
+            totalFollowers
+            totalFollowing
+            totalPosts
+            totalComments
+            totalMirrors
+            totalPublications
+            totalCollects
+            }
+            followModule {
+            ... on FeeFollowModuleSettings {
+                type
+                amount {
+                asset {
+                    symbol
+                    name
+                    decimals
+                    address
+                }
+                value
+                }
+                recipient
+            }
+            ... on ProfileFollowModuleSettings {
+            type
+            }
+            ... on RevertFollowModuleSettings {
+            type
+            }
+        }
+        }
+    }
+`;
