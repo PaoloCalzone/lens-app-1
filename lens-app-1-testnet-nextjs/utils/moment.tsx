aaf9370a02b4c05877b59f1363f5cfea0f3d64ed
paolocalzone <paolocalzone@protonmail.com>
2022-07-04 17:11:37 +0200

    Set created time

--- lens-app-1-testnet-nextjs/utils/moment.tsx
+++ lens-app-1-testnet-nextjs/utils/moment.tsx
@@ -2,7 +2,7 @@ import moment from "moment";
 
 export function isOlderThan24H(date) {
   const now = moment();
-  const createdAt = moment(date).format("dddd, MMMM Do ,");
+  const createdAt = moment(date).format("dddd, MMMM Do h:mm:ss a,");
   console.log("AGE", now.diff(date, "days"));
   return now.diff(date, "days") > 1;
 }
