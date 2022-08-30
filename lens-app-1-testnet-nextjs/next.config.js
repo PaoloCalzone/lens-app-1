2f0e5e6e55bcf3e2d32d1bda7518b24cea6210a5
paolocalzone <paolocalzone@protonmail.com>
2022-08-30 21:41:06 +0200

    Edit images hosts

--- lens-app-1-testnet-nextjs/next.config.js
+++ lens-app-1-testnet-nextjs/next.config.js
@@ -2,7 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["ipfs.infura.io", "avatar.tobi.sh"],
+    domains: [
+      "ipfs.infura.io",
+      "avatar.tobi.sh",
+      "lens.infura-ipfs.io",
+      "statics-mumbai-lens-staging.s3.eu-west-1.amazonaws.com",
+    ],
   },
 };
 
