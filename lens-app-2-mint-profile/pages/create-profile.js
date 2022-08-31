6c8b2fac6c4b9b8e4ae02ec4b796042c9cc8bdca
calzoneSurface <calzoneSurface@protonmail.com>
2022-08-31 19:31:06 +0200

    Eslint prettier

--- lens-app-2-mint-profile/pages/create-profile.js
+++ lens-app-2-mint-profile/pages/create-profile.js
@@ -8,11 +8,33 @@ export default function CreateProfile() {
   const [followNFTURI, setFollowNFTURI] = useState(null);
   const [followModule, setFollowModule] = useState(null);
 
+  function handleSubmit() {
+    console.log("Handle is", handle);
+  }
+
   return (
     <div>
       <section>
         <form onSubmit={handleSubmit}>
-          <div></div>
+          <div className="flex-col">
+            <label htmlFor="handle">Handle</label>
+            <input
+              id="handle"
+              name="handle"
+              type="text"
+              required
+              value={handle}
+              onChange={(e) => setHandle(e.target.value)}
+            />
+            <label htmlFor="pictureUri">Avatar</label>
+            <input
+              type="text"
+              name="profilePictureURI"
+              onChange={(e) => {
+                setProfilePictureUri(e.target.value);
+              }}
+            />
+          </div>
         </form>
       </section>
     </div>
